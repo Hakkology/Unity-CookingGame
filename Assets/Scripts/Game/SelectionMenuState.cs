e35bd2c58141b7f56614bdf03c9ac6e3a8ac5166
Hakkology <yildizhakan88@gmail.com>
2024-04-13 11:43:31 +0300

    Gamestates.

---
+++ Assets/Scripts/Game/SelectionMenuState.cs
@@ -0,0 +1,6 @@
+public class SelectionMenuState : IGameState
+{
+    public void EnterState() { /* Menüyü göster */ }
+    public void ExitState() { /* Menüyü kaldır */ }
+    public void UpdateState() { /* Input kontrolü */ }
+}
