fa3e3015920dcc0a20679f729a85e5180b5f1354
RevoUKC <utkukaancomert@gmail.com>
2024-05-01 14:28:37 +0300

    deneme push

--- Assets/Scripts/Player/MovementConstants.cs
+++ Assets/Scripts/Player/MovementConstants.cs
@@ -1,9 +1,9 @@
 public static class MovementConstants
 {
     public const float MaxForce = 40f;
-    public const float MaxMoveSpeed = 8.0f;
-    public const float MaxRotationSpeed = 720.0f;
-    public const float MaxJumpForce = 60f;
+    public const float MaxMoveSpeed = 15.0f;
+    public const float MaxRotationSpeed = 1800.0f;
+    public const float MaxJumpForce = 100f;
 
     public const float WaterMoveSpeed = 5f; 
     public const float WaterForce = 80f;
@@ -13,7 +13,7 @@ public static class MovementConstants
     public const float WaterResistanceForce = 60f;
 
     public const float SlideMoveSpeed = 8.0f;
-    public const float SlideEfficiencyConstant = 0.1f;
+    public const float SlideEfficiencyConstant = 0.3f;
     public const float SlideMinSpeed = 5f;
     public const float SlideRotationSpeed = 720f;
 
